c2e314fe059130db656896e1bce2258ab00a6d48
ahmedadegbami <ahmed.adegbami@gmail.com>
2022-07-01 14:43:40 +0200

    authorization done for both user and accomodation

--- src/apis/accomodations/index.js
+++ src/apis/accomodations/index.js
@@ -1,19 +1,27 @@
 import express from "express";
 import createError from "http-errors";
 import accomdationModel from "./model.js";
+import { hostOnlyMiddleware } from "../../auth/host.js";
+import { JWTAuthMiddleware } from "../../auth/token.js";
+import { generateAccessToken } from "../../auth/tools.js";
 
 const accomdationRouter = express.Router();
 
-accomdationRouter.post("/", async (req, res, next) => {
-  try {
-    const newAccomdation = new accomdationModel(req.body);
-    const { _id } = await newAccomdation.save();
-    res.status(201).send({ _id });
-  } catch (error) {
-    console.log(error);
-    next(error);
+accomdationRouter.post(
+  "/",
+  JWTAuthMiddleware,
+  hostOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const newAccomdation = new accomdationModel(req.body);
+      const { _id } = await newAccomdation.save();
+      res.status(201).send({ _id });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
   }
-});
+);
 
 accomdationRouter.get("/", async (req, res, next) => {
   try {
@@ -25,66 +33,81 @@ accomdationRouter.get("/", async (req, res, next) => {
   }
 });
 
-accomdationRouter.get("/:accomdationId", async (req, res, next) => {
-  try {
-    const accomdation = await accomdationModel.findById(
-      req.params.accomdationId
-    );
-    if (accomdation) {
-      res.send(accomdation);
-    } else {
-      next(
-        createError(
-          404,
-          `Accomdation with id ${req.params.accomdationId} not found!`
-        )
+accomdationRouter.get(
+  "/:accomdationId",
+  JWTAuthMiddleware,
+  hostOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const accomdation = await accomdationModel.findById(
+        req.params.accomdationId
       );
+      if (accomdation) {
+        res.send(accomdation);
+      } else {
+        next(
+          createError(
+            404,
+            `Accomdation with id ${req.params.accomdationId} not found!`
+          )
+        );
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-accomdationRouter.put("/:accomdationId", async (req, res, next) => {
-  try {
-    const updatedAccomdation = await accomdationModel.findByIdAndUpdate(
-      req.params.accomdationId,
-      req.body,
-      { new: true, runValidators: true }
-    );
-    if (updatedAccomdation) {
-      res.send(updatedAccomdation);
-    } else {
-      next(
-        createError(
-          404,
-          `Accomdation with id ${req.params.accomdationId} not found!`
-        )
+accomdationRouter.put(
+  "/:accomdationId",
+  JWTAuthMiddleware,
+  hostOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const updatedAccomdation = await accomdationModel.findByIdAndUpdate(
+        req.params.accomdationId,
+        req.body,
+        { new: true, runValidators: true }
       );
+      if (updatedAccomdation) {
+        res.send(updatedAccomdation);
+      } else {
+        next(
+          createError(
+            404,
+            `Accomdation with id ${req.params.accomdationId} not found!`
+          )
+        );
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-accomdationRouter.delete("/:accomdationId", async (req, res, next) => {
-  try {
-    const deletedAccomdation = await accomdationModel.findByIdAndDelete(
-      req.params.accomdationId
-    );
-    if (deletedAccomdation) {
-      res.send(deletedAccomdation);
-    } else {
-      next(
-        createError(
-          404,
-          `Accomdation with id ${req.params.accomdationId} not found!`
-        )
+accomdationRouter.delete(
+  "/:accomdationId",
+  JWTAuthMiddleware,
+  hostOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const deletedAccomdation = await accomdationModel.findByIdAndDelete(
+        req.params.accomdationId
       );
+      if (deletedAccomdation) {
+        res.send(deletedAccomdation);
+      } else {
+        next(
+          createError(
+            404,
+            `Accomdation with id ${req.params.accomdationId} not found!`
+          )
+        );
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 export default accomdationRouter;
--- src/apis/users/index.js
+++ src/apis/users/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import createError from "http-errors";
 import userModel from "./model.js";
+import accomodationModel from "../accomodations/model.js";
+import { hostOnlyMiddleware } from "../../auth/host.js";
+import { JWTAuthMiddleware } from "../../auth/token.js";
+import { generateAccessToken } from "../../auth/tools.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/", async (req, res, next) => {
+userRouter.post("/register", async (req, res, next) => {
   try {
     const newUser = new userModel(req.body);
     const { _id } = await newUser.save();
@@ -14,9 +18,61 @@ userRouter.post("/", async (req, res, next) => {
   }
 });
 
+userRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const me = await userModel.findById(req.user._id);
+    res.send(me);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// rerturn the full list of accomodation posted by a host
+userRouter.get(
+  "/me/accomodations",
+  JWTAuthMiddleware,
+  hostOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const me = await userModel.findById(req.user._id);
+      if (me.role === "host") {
+        const accomdations = await accomodationModel.find({
+          host: req.user._id
+        });
+        res.send(accomdations);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+userRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const modifiedUser = await userModel.findByIdAndUpdate(
+      req.user._id,
+      req.body,
+      { new: true }
+    );
+    res.send(modifiedUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+userRouter.delete("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    await userModel.findByIdAndDelete(req.user._id);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+});
+
 userRouter.get(
   "/",
-
+  JWTAuthMiddleware,
+  hostOnlyMiddleware,
   async (req, res, next) => {
     try {
       const users = await userModel.find();
@@ -82,5 +138,25 @@ userRouter.delete(
     }
   }
 );
+
+userRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.checkCredentials(email, password);
+
+    if (user) {
+      const accessToken = await generateAccessToken({
+        _id: user._id,
+        role: user.role
+      });
+      res.send({ accessToken });
+    } else {
+      next(createError(401, "Credentials are not ok!"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default userRouter;
